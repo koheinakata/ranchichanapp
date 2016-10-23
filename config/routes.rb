cbb7ef7d9ec8c9658bb1afab8b0a5dd65561f3fb
Kouhei Nakata <koheinakata0508@gmail.com>
2016-10-23 18:01:27 +0900

    conseptページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :users
   root to: "tweets#index"
   resources :tweets, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
@@ -9,6 +13,8 @@ end
 
 
 #                   Prefix Verb   URI Pattern                          Controller#Action
+#        static_pages_home GET    /static_pages/home(.:format)         static_pages#home
+#        static_pages_help GET    /static_pages/help(.:format)         static_pages#help
 #         new_user_session GET    /users/sign_in(.:format)             devise/sessions#new
 #             user_session POST   /users/sign_in(.:format)             devise/sessions#create
 #     destroy_user_session DELETE /users/sign_out(.:format)            devise/sessions#destroy
